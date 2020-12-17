35dae27abd1db357a6c957d4522029ab95e6c275
sojung127 <ssojung127@gmail.com>
2020-12-17 15:13:49 +0900

    create project

---
+++ Algorithm_graph/Algorithm_graph/tomato.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	int m, n, h;	// m: 상자의 가로칸 수, n: 세로칸, h: 높이 
+	scanf("%d %d %", &m, &n, &h);
+
+
+
+
+
+
+	return 0;
+}
